8f4978d0684f7f232c484cc498ac8802eb9dcc35
Jason K. Jackson <jason.jackson@monster.com>
2009-04-10 03:05:09 +0000

    removed the repos and calls to install gems
    i dont want to be fetching from random third party repo's to make shit work

--- bin/install_on_centos_with_gems.sh
+++ bin/install_on_centos_with_gems.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
-wget http://source.collectiveidea.com/pub/linux/centos/4/i386/ci.repo -O /etc/yum.repos.d/ci.repo
-rpm -Uvh http://download.fedora.redhat.com/pub/epel/5/i386/epel-release-5-3.noarch.rpm
+#wget http://source.collectiveidea.com/pub/linux/centos/4/i386/ci.repo -O /etc/yum.repos.d/ci.repo
+#rpm -Uvh http://download.fedora.redhat.com/pub/epel/5/i386/epel-release-5-3.noarch.rpm
 
-yum install -y ruby rubygems
+#yum install -y ruby rubygems
 
-wget http://rubyworks.rubyforge.org/RubyWorks.repo -O /etc/yum.repos.d/RubyWorks.repo
+#wget http://rubyworks.rubyforge.org/RubyWorks.repo -O /etc/yum.repos.d/RubyWorks.repo
 
-yum install -y ruby gcc gcc-c++ ruby-devel ruby-docs ruby-ri ruby-irb ruby-rdoc
+#yum install -y ruby gcc gcc-c++ ruby-devel ruby-docs ruby-ri ruby-irb ruby-rdoc
 
-gem sources -d http://rubyworks.rubyforge.org/redhat/5/GEMS/i386/
-gem sources -a http://gems.rubyforge.org
+#gem sources -d http://rubyworks.rubyforge.org/redhat/5/GEMS/i386/
+#gem sources -a http://gems.rubyforge.org
 
-cd /tmp
-wget http://rubyforge.org/frs/download.php/45904/rubygems-update-1.3.1.gem
-gem install rubygems-update
-update_rubygems
+#cd /tmp
+#wget http://rubyforge.org/frs/download.php/45904/rubygems-update-1.3.1.gem
+#gem install rubygems-update
+#update_rubygems
 
-gem sources -a http://gems.opscode.com
-gem install chef ohai chef-server
+#gem sources -a http://gems.opscode.com
+#gem install chef ohai chef-server
 
 mkdir /etc/chef
 
